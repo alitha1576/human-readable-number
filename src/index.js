be7c26c345b5e6e71c4c9a2c7ad63a4efa518f40
Anna Petrova <ann.undine@gmail.com>
2024-08-19 10:52:45 +0200

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function toReadable (number) {
-  
+    const units = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    const teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    const tens = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+    const hundreds = ["", "one hundred", "two hundred", "three hundred", "four hundred", "five hundred", "six hundred", "seven hundred", "eight hundred", "nine hundred"];
+
+    if (number < 10) {
+        return units[number];
+    }
+    else if (number < 20) {
+        return teens[number - 10];
+    }
+
+    else if (number < 100) {
+        return tens[Math.floor(number / 10)] + (number % 10 !== 0 ? " " + units[number % 10] : "");
+    }
+
+    else if (number < 1000) {
+        return hundreds[Math.floor(number / 100)] + (number % 100 !== 0 ? " " + toReadable(number % 100) : "");
+    }
+    else {
+        return 'too big number';
+    }
 }
